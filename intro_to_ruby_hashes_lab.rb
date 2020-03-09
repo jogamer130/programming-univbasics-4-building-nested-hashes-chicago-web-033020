2b4952c0a1bb614de405465a78b55b9c2327f487
Derek Williams <jogamer130@gmail.com>
2020-03-09 01:48:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,7 +44,7 @@ def monopoly_with_third_tier
       },
       :names => {
         :reading_railroad => {},
-        :reading_railroad => {},
+        :pennsylvania_railroad => {},
         :reading_railroad => {},
         :reading_railroad => {}
       }
